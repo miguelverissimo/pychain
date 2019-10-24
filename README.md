6f2e1d493d4cf3d700966b21db0004e5b431239d
Miguel Verissimo <mverissimo@pivotal.io>
2019-10-24 13:49:51 -0400

    Add tests.

---
+++ README.md
@@ -0,0 +1,18 @@
+### Activate the virtual environment
+
+```
+source pychain-env/bin/activate
+```
+
+### Install dependencies
+
+```
+pip3 install -r requirements.txt
+```
+
+### Run the tests
+
+```
+source pychain-env/bin/activate
+python3 -m pytest backend/tests
+```
--- backend/blockchain/block.py
+++ backend/blockchain/block.py
@@ -1,5 +1,12 @@
 import time
-from crypto_hash import crypto_hash
+from backend.util.crypto_hash import crypto_hash
+
+GENESIS_DATA = {
+    'timestamp': 1,
+    'prev_hash': 'genesis_previous_hash',
+    'hash': 'genesis_hash',
+    'data': []
+}
 
 
 class Block:
@@ -36,7 +43,7 @@ class Block:
         """
         Genesis generates the genesis (initial) block
         """
-        return Block(1, 'genesis_previous_hash', 'genesis_hash', [])
+        return Block(**GENESIS_DATA)
 
 
 def main():
